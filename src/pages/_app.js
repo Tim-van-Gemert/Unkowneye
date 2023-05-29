8b81e4b3f0125b974fdbae469535a6dbfba6402e
Tim van Gemert <timvangemert@outlook.com>
2023-05-29 16:51:14 +0200

    Added SalamanderKip to UNKOWNEYE

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import useFetchAllPlayers from "./api/useFetchAllPlayers";
 
 export default function App({ Component, pageProps }) {
   
-  const { playersStats } = useFetchAllPlayers([['account.bf0458c4350a4f58acfac101b3bd1df2', 'TIMMAHHH'], ['account.56162efa45c846cd98eae1a741b338b7', 'JustDaanGames']]);
+  const { playersStats } = useFetchAllPlayers([['account.bf0458c4350a4f58acfac101b3bd1df2', 'TIMMAHHH'], ['account.56162efa45c846cd98eae1a741b338b7', 'JustDaanGames'], ['account.aded6e78e0234600b8d1bccfc87865f9', 'SalamanderKip']]);
 
   return (
     <>
