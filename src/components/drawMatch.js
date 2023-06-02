68acf576fac435de1addfc29f84bccb802dbae97
Tim van Gemert <timvangemert@outlook.com>
2023-06-02 15:18:21 +0200

    changed backround logic

--- src/components/drawMatch.js
+++ src/components/drawMatch.js
@@ -1,12 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 
+
 export default function drawMatch({ telemetryData }) {
     const canvasRef = useRef(null);
     const [showMap, setShowMap] = useState(false)            
-    //CHANGE THIS TO CURRENT MAP
-    const background = new Image();
-    background.src = "/erangel.png";
-
 
     const processPlayer = (ctx, RawX, RawY, rawPrevX, rawPrevY) => {
         // 8X8
@@ -37,7 +34,6 @@ export default function drawMatch({ telemetryData }) {
         ctx.arc(x, y, 10, 0, 2 * Math.PI);
         ctx.fill();
         
-        // ctx.drawImage(background, 0, 0);
         // if (prevX != null || prevY != null) {
         //     ctx.clearRect(prevX - 10, prevY - 10, 20, 20);
         // }
@@ -49,7 +45,7 @@ export default function drawMatch({ telemetryData }) {
             console.log(telemetryData)
 
             // Make sure the image is loaded first otherwise nothing will draw.
-            background.onload = function () {
+
                 const canvas = canvasRef.current;
                 const ctx = canvas.getContext('2d');
                 let rawPrevX = null;
@@ -58,13 +54,13 @@ export default function drawMatch({ telemetryData }) {
                 for (let i = 0; i < telemetryData.length; i++) {
                     if (telemetryData[i]?.common?.isGame == 1 && telemetryData[i]?.character) {
                         if (telemetryData[i]?.character?.location) {
-                            setTimeout(function () {
+                            // setTimeout(function () {
                                 const RawX = telemetryData[i].character.location.x;
                                 const RawY = telemetryData[i].character.location.y;
                                 processPlayer(ctx, RawX, RawY, rawPrevX, rawPrevY)
                                 rawPrevX = RawX;
                                 rawPrevY = RawY;
-                            }, 100);
+                            // }, 100);
 
                         }
                     }
@@ -72,7 +68,6 @@ export default function drawMatch({ telemetryData }) {
                     setShowMap(true)
 
                 }
-            }
         }
 
         // Use the telemetryData to draw the canvas content
@@ -82,7 +77,7 @@ export default function drawMatch({ telemetryData }) {
 
     return (
         <div className={`w-full  h-full  ${showMap ? 'flex' : 'hidden'} justify-start align-items p-5`}>
-            <canvas className={`object-cover w-[500px] ${showMap ? 'flex' : 'hidden'} rounded-2xl h-[500px]  `} ref={canvasRef} width={1400} height={1400} />
+            <canvas className={`object-cover w-[500px] bg-cover bg-[url('/erangel.png')] ${showMap ? 'flex' : 'hidden'} rounded-2xl h-[500px] `} ref={canvasRef} width={1400} height={1400} />
         </div>
     );
 
