4c21cea5df0491490b41a6a32d5b8ff7ce86287a
Tim van Gemert <timvangemert@outlook.com>
2023-06-05 12:49:34 +0200

    changed header

--- src/components/header.js
+++ src/components/header.js
@@ -13,11 +13,14 @@ export default function Header () {
     return (
         <>
             <motion.h1 
-            className="font-sec sm:p-10 p-4  bg-[url('/header2.jpeg')] bg-cover bg-center text-white font-thin text-[28px] sm:text-[50px]  text-center lg:text-start "
+            className="font-sec bg-[url('/header3.jpeg')] bg-cover bg-center  "
             initial="hidden"
             animate="visible"
             variants={staggerVariants}>
+            <div class="w-full h-full flex sm:p-10 p-4  justify-center sm:justify-start text-white font-thin text-[28px] sm:text-[50px]  text-center lg:text-start  backdrop-brightness-50">
             UNKNOWNEYE
+
+            </div>
             </motion.h1>
 
         </>
